b6bcc2b4f0beb77f58b9890f0c9b6524f57f37b9
Paul Gevers <elbrus@debian.org>
2017-07-26 21:25:28 -0400

    Update pinning values to APT defaults

--- lib/adt_testbed.py
+++ lib/adt_testbed.py
@@ -1189,9 +1189,9 @@ fi
                 ' '.join(srcpkgs)
 
         # prefer given packages from pocket, other packages from
-        # default $REL (prio 900), but make $REL-pocket available for
-        # dependency resolution (prio 800)
-        script += 'printf "Package: $PKGS\\nPin: release a=${REL}-%(pocket)s\\nPin-Priority: 990\\n\\nPackage: *\\nPin: release a=$REL\\nPin-Priority: 900\\n\\nPackage: *\\nPin: release a=${REL}-updates\\nPin-Priority: 900\\n\\nPackage: *\\nPin: release a=${REL}-%(pocket)s\\nPin-Priority: 800\\n" > /etc/apt/preferences.d/autopkgtest-${REL}-%(pocket)s; ' % \
+        # default $REL (prio 990), but make $REL-pocket available for
+        # dependency resolution (prio 500)
+        script += 'printf "Package: $PKGS\\nPin: release a=${REL}-%(pocket)s\\nPin-Priority: 995\\n\\nPackage: *\\nPin: release a=$REL\\nPin-Priority: 990\\n\\nPackage: *\\nPin: release a=${REL}-updates\\nPin-Priority: 990\\n\\nPackage: *\\nPin: release a=${REL}-%(pocket)s\\nPin-Priority: 500\\n" > /etc/apt/preferences.d/autopkgtest-${REL}-%(pocket)s; ' % \
             {'pocket': pocket}
         self.check_exec(['sh', '-ec', script])
         self.apt_pin_for_pockets.append(pocket)
